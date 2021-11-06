85d9c26304b455fc23e93811519650817084fe5e
howkmii <abdelhakim@univ-tiaret.dz>
2021-11-06 13:13:26 +0100

    first commit

---
+++ PCA.py
@@ -0,0 +1,6 @@
+import numpy as np                         # Linear algebra library
+import matplotlib.pyplot as plt            # library for visualization
+from sklearn.decomposition import PCA      # PCA library
+import pandas as pd                        # Data frame library
+import math                                # Library for math functions
+import random                              # Library for pseudo random numbers
