f45f1c23b8f66d9f16fb265e1afac967c5452ea6
howkmii <abdelhakim@univ-tiaret.dz>
2021-11-06 13:19:49 +0100

    Correlated Normal Random Variables

--- PCA.py
+++ PCA.py
@@ -71,3 +71,34 @@ data = np.dot(xy, rotationMatrix) # Return a nD array
 # Print the rotated data
 plt.scatter(data[:,0], data[:,1])
 plt.show()
+
+
+plt.scatter(data[:,0], data[:,1]) # Print the original data in blue
+
+# Apply PCA. In theory, the Eigenvector matrix must be the 
+# inverse of the original rotationMatrix. 
+pca = PCA(n_components=2)  # Instantiate a PCA. Choose to get 2 output variables
+
+# Create the transformation model for this data. Internally it gets the rotation 
+# matrix and the explained variance
+pcaTr = pca.fit(data)
+
+# Create an array with the transformed data
+dataPCA = pcaTr.transform(data)
+
+print('Eigenvectors or principal component: First row must be in the direction of [1, n]')
+print(pcaTr.components_)
+
+print()
+print('Eigenvalues or explained variance')
+print(pcaTr.explained_variance_)
+
+# Print the rotated data
+plt.scatter(dataPCA[:,0], dataPCA[:,1])
+
+# Plot the first component axe. Use the explained variance to scale the vector
+plt.plot([0, rotationMatrix[0][0] * std1 * 3], [0, rotationMatrix[0][1] * std1 * 3], 'k-', color='red')
+# Plot the second component axe. Use the explained variance to scale the vector
+plt.plot([0, rotationMatrix[1][0] * std2 * 3], [0, rotationMatrix[1][1] * std2 * 3], 'k-', color='green')
+
+plt.show()
