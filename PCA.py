4b5ea141f4ba5a4612343b3d26df97e5e5a7174e
howkmii <abdelhakim@univ-tiaret.dz>
2021-11-06 13:16:05 +0100

    To start, let us consider a pair of random variables x, y. Consider the base case when y = n * x. The x and y variables will be perfectly correlated to each other since y is just a scaling of x

--- PCA.py
+++ PCA.py
@@ -4,3 +4,21 @@ from sklearn.decomposition import PCA      # PCA library
 import pandas as pd                        # Data frame library
 import math                                # Library for math functions
 import random                              # Library for pseudo random numbers
+
+
+n = 1  # The amount of the correlation
+x = np.random.uniform(1,2,1000) 
+y = x.copy() * n 
+x = x - np.mean(x) 
+y = y - np.mean(y) 
+
+data = pd.DataFrame({'x': x, 'y': y}) 
+plt.scatter(data.x, data.y) 
+pca = PCA(n_components=2) # 
+pcaTr = pca.fit(data)
+
+rotatedData = pcaTr.transform(data)
+dataPCA = pd.DataFrame(data = rotatedData, columns = ['PC1', 'PC2']) 
+# Plot the transformed data in orange
+plt.scatter(dataPCA.PC1, dataPCA.PC2)
+plt.show()
