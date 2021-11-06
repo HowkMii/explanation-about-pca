52e7a6e6f6fec930b7ed70f744d3d7ef52cee54c
howkmii <abdelhakim@univ-tiaret.dz>
2021-11-06 13:18:51 +0100

    Correlated Normal Random Variables

--- PCA.py
+++ PCA.py
@@ -31,3 +31,43 @@ print(pcaTr.components_)
 print()
 print('Eigenvalues or explained variance')
 print(pcaTr.explained_variance_)
+
+
+
+import matplotlib.lines as mlines
+import matplotlib.transforms as mtransforms
+
+random.seed(100)
+
+std1 = 1     # The desired standard deviation of our first random variable
+std2 = 0.333 # The desired standard deviation of our second random variable
+
+x = np.random.normal(0, std1, 1000) # Get 1000 samples from x ~ N(0, std1)
+y = np.random.normal(0, std2, 1000)  # Get 1000 samples from y ~ N(0, std2)
+#y = y + np.random.normal(0,1,1000)*noiseLevel * np.sin(0.78)
+
+# PCA works better if the data is centered
+x = x - np.mean(x) # Center x 
+y = y - np.mean(y) # Center y
+
+#Define a pair of dependent variables with a desired amount of covariance
+n = 1 # Magnitude of covariance. 
+angle = np.arctan(1 / n) # Convert the covariance to and angle
+print('angle: ',  angle * 180 / math.pi)
+
+# Create a rotation matrix using the given angle
+rotationMatrix = np.array([[np.cos(angle), np.sin(angle)],
+                 [-np.sin(angle), np.cos(angle)]])
+
+
+print('rotationMatrix')
+print(rotationMatrix)
+
+xy = np.concatenate(([x] , [y]), axis=0).T # Create a matrix with columns x and y
+
+# Transform the data using the rotation matrix. It correlates the two variables
+data = np.dot(xy, rotationMatrix) # Return a nD array
+
+# Print the rotated data
+plt.scatter(data[:,0], data[:,1])
+plt.show()
